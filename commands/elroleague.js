4f8960fe9479e7278e736394db0fabb677310f4f
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-04-01 00:42:48 +0200

    Update

--- commands/elroleague.js
+++ commands/elroleague.js
@@ -1,5 +1,8 @@
+var EloRank = require('elo-rank');
+var elo = new EloRank(50);
+
 module.exports = {
-	name: 'elroligue',
+	name: 'elroleague',
 	async execute(message, args) {
 		message.delete({ timeout: 100 });
 		if (message.member.roles.cache.has('646700553648340993' || '646687065597149189')) {
@@ -20,7 +23,82 @@ module.exports = {
 			message.channel.send(exampleEmbed);
 		}
 		if(message.mentions.users.first()){
-			let player = message.mentions.users.first();
+			const player = message.mentions.users.first();
+			if(args[1]){
+				console.log(args)
+				console.log(message.mentions.users.first())
+				let count = 0;
+				let user2;
+				message.mentions.users.forEach(user => {
+					console.log(user)
+					count++
+					if (count === 1) return;
+					else user2 = user
+				});
+				console.log(user2);
+
+				const Player1 = await Rank.findOneAndUpdate(
+					{ player: player.username },
+					{
+						
+					},
+					{ new: true, upsert: true }
+				 )
+				
+				// const Player2 = await Rank.findOne({ player: user2});
+
+				const Player2 = await Rank.findOneAndUpdate(
+					{ player: user2.username },
+					{
+						
+					},
+					{ new: true, upsert: true }
+				 )
+
+				console.log(Player1);
+				console.log(Player2);
+
+				//Gets expected score for first parameter
+				const expectedScoreA = elo.getExpected(Player1.point, Player2.point);
+				const expectedScoreB = elo.getExpected(Player2.point, Player1.point);
+				
+				console.log(expectedScoreA);
+				console.log(expectedScoreB);
+
+				//update score, 1 if won 0 if lost
+				newScoreplayerA = elo.updateRating(expectedScoreA, 1, Player1.point);
+				newScoreplayerB = elo.updateRating(expectedScoreB, 0, Player2.point);
+				
+				console.log(newScoreplayerA);
+				console.log(newScoreplayerB);
+
+				message.channel.send(`Nouveau Match dans la Elroligue !\n\n${player.toString()} passe de ${Player1.point} points à ${newScoreplayerA} points\n${user2.toString()} passe de ${Player2.point} points à ${newScoreplayerB} points`)
+
+				await Rank.findOneAndUpdate(
+					{ player: player.username },
+					{
+						$inc: { win: 1},
+						point: newScoreplayerA
+					},
+					{ new: true, upsert: true }
+				 )
+				.then((info) => console.log(info))
+				.catch((error) => console.log(error))
+
+				await Rank.findOneAndUpdate(
+					{ player: user2.username },
+					{
+						$inc: { lose: 1},
+						point: newScoreplayerB
+					},
+					{ new: true, upsert: true }
+				 )
+				.then((info) => console.log(info))
+				.catch((error) => console.log(error))
+
+
+			}
+			else {
 			if (args[0] === "delete"){
 				await Rank.deleteOne({ player: player.username})
 				.then((info) => console.log(info))
@@ -56,6 +134,7 @@ module.exports = {
 				.then((info) => console.log(info))
 				.catch((error) => console.log(error))
 			}
+			}
 		}
 
 		// console.log(Rank.find({}))
@@ -74,10 +153,10 @@ module.exports = {
 		})
 
 		//CHANNEL ID
-		let gdaChannel = message.guild.channels.cache.get("826797824820772895")
+		let gdaChannel = message.guild.channels.cache.get("818445025410809887")
 		
 		// //MESSAGE ID
-		gdaChannel.messages.fetch("826798088201961534")
+		gdaChannel.messages.fetch("826714566050054144")
 		.then(msg => {
 
 				const newEmbed = new Discord.MessageEmbed()
