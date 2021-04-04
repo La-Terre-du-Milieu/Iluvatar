473c7d2fafeef71920cc0758e0f61f8cd393044d
elrohir-bfme <53747262+elrohir-bfme@users.noreply.github.com>
2021-04-04 21:02:49 +0200

    up

--- commands/sacrifie.js
+++ commands/sacrifie.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'sacrifie',
 	async execute(message, args) {
 		// message.delete({ timeout: 100 });
-		if (message.member.roles.cache.has('646700553648340993' || '646687065597149189')) {
+		// if (message.member.roles.cache.has('646700553648340993' || '646687065597149189')) {
 			let sacrifie = []
 			message.mentions.users.forEach(user => {
 				console.log(user)
@@ -11,6 +11,6 @@ module.exports = {
 			let playerToSacrifie = sacrifie[Math.floor(Math.random() * sacrifie.length)]
 			message.channel.send("Attention...")
 			message.channel.send(`Le joueur sacrifié est ${playerToSacrifie.toString()}`)
-		}
+		// }
 	},
 };
