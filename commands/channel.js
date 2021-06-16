eb1e4638771d7614094983e696de7e268ced2964
Paul Rassant <elrohir.rassant@gmail.com>
2021-06-16 15:48:58 +0200

    Update

---
+++ commands/channel.js
@@ -0,0 +1,43 @@
+const reader = require("g-sheets-api");
+const fetch = require("node-fetch");
+module.exports = {
+	name: 'supertest',
+	description: 'supertest',
+	async execute(message) {
+        // const readerOptions = {
+        //     sheetId: "1innwKbd8HFpXSF3FVH90NiIG5nCACNHf587Vxw1bN3E",
+        //     returnAllResults: true
+        //   };
+
+
+        //   https://api.npoint.io/38a2899b98818d89418c
+
+            await fetch("https://api.npoint.io/38a2899b98818d89418c")
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data)
+                console.log("dedededede")
+                data.map(region => {
+                    console.log(region)
+                    console.log(region.hasOwnProperty("fight"))
+                    if(region.hasOwnProperty("fight")) {
+                        console.log(region)
+                    }
+                })
+
+            }).catch(err => {
+                // Do something for an error here
+            });
+          
+        //   reader(readerOptions, (results) => {
+              
+        //       results.map(result => {
+
+        //         console.log(result)
+        //         console.log(result['Pseudo GDA'])
+        //       })
+            
+		//         // message.reply(results)   
+        //   }); 
+    },
+};
--- commands/help.js
+++ commands/help.js
@@ -4,14 +4,14 @@ module.exports = {
 	name: 'help',
 	description: 'List all available commands.',
 	execute(message) {
-		let str = '';
-		const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+		// let str = '';
+		// const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-		for (const file of commandFiles) {
-			const command = require(`./${file}`);
-			str += `Name: ${command.name}\n, Description: ${command.description} \n`;
-		}
+		// for (const file of commandFiles) {
+		// 	const command = require(`./${file}`);
+		// 	str += `Name: ${command.name}\n, Description: ${command.description} \n`;
+		// }
 
-		message.channel.send(str);
+		// message.channel.send(str);
 	},
 };
