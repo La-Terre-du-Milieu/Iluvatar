52db6438cffe3b61175409269b339624b47408a1
elrohir-bfme <elrohir.rassant@gmail.com>
2022-04-16 11:43:20 +0200

    Update test

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const fs = require('fs');
+const cron = require('cron');
+
 global.mongoose = require('mongoose');
 mongoose.set('useFindAndModify', false);
 require('dotenv').config();
 // global.strapi = mongoose.createConnection(process.env.dataURLStrapi, { useNewUrlParser: true, useUnifiedTopology: true });
 global.ltdm = mongoose.createConnection(process.env.dataURL, { useNewUrlParser: true, useUnifiedTopology: true });
 
-
+const qs = require('qs');
+const axios = require('axios').default;
+const moment = require('moment');
 // // eslint-disable-next-line no-undef
 // mongoose.connect(process.env.dataURL, { useNewUrlParser: true, useUnifiedTopology: true });
 // // eslint-disable-next-line no-undef
@@ -26,10 +30,60 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
+function delay(t, val) {
+	return new Promise(function(resolve) {
+		setTimeout(function() {
+			resolve(val);
+		}, t);
+	});
+}
+
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('Ready!');
+
+	let scheduledMessage = new cron.CronJob('00 38 11 * * *', async () => {
+			const guild = client.guilds.cache.get('646686225737973770');
+			const channel = guild.channels.cache.get('646699608138973204');
+
+			const query = qs.stringify({
+				populate: '*',
+				filters: {
+					date: {
+					$eq: moment(new Date()).format("DD/MM"),
+					},
+				},
+			}, {
+			encodeValuesOnly: true,
+			});
+
+			console.log(moment(new Date()).format("DD/MM"))
+
+			const { data } = await axios.get(`https://api.laterredumilieu.fr/api/tdms?${query}`); 
+			console.log(data) 
+
+			if(data && data.data && data.data.length > 0){
+				for (let information of data.data) {
+					console.log(information.attributes)
+					let TDM = information.attributes
+					console.log(TDM, TDM.img.data.attributes.url);
+					let embed = new Discord.MessageEmbed()
+					.setTitle(TDM.title)
+					.setDescription(TDM.desc)
+					.setColor("#ff6347")
+					.setImage(`https://api.laterredumilieu.fr${TDM.img.data.attributes.url}`)
+					.setAuthor(`ANNÉE ${TDM.year}`)
+					.setFooter(`Aujourd'hui en Terre du Milieu • ${TDM.date}`);
+					channel.send(embed)
+					await delay(1800000);
+				}
+			}
+		});
+
+		// When you want to start it, use:
+		scheduledMessage.start()
+
 	// client.users.cache.find(user => user.id === 'USER-ID')
 	// const user = client.users.fetch('931280660746879066').then(member => {
 
