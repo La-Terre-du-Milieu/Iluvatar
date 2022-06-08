95463b15c50473da641fc90e50e9eea6dda5b136
elrohir-bfme <elrohir.rassant@gmail.com>
2022-06-08 18:43:20 +0200

    oo

---
+++ commands/lfa.js
@@ -0,0 +1,40 @@
+module.exports = {
+	name: 'bobby',
+	description: 'bobby',
+	execute(message) {
+        const guild = client.guilds.cache.get('646686225737973770');
+			const channel = guild.channels.cache.get('646686225737973773');
+
+			const query = qs.stringify({
+				populate: '*',
+                pagination: {
+                    page: 1,
+                    pageSize: 50,
+                },
+			}, {
+			encodeValuesOnly: true,
+			});
+
+			console.log(moment(new Date()).format("DD/MM"))
+
+			const { data } = await axios.get(`https://api.laterredumilieu.fr/api/players?${query}`); 
+			console.log(data) 
+
+			if(data && data.data && data.data.length > 0){
+				for (let player of data.data) {
+					console.log(player.attributes)
+					let p = player.attributes;
+					let embed = new Discord.MessageEmbed()
+					.setTitle(TDM.title)
+					.setDescription(TDM.desc)
+					.setColor("#ff6347")
+					.setImage(`https://api.laterredumilieu.fr${TDM.img.data.attributes.url}`)
+					.setAuthor(`ANNÉE ${TDM.year}`)
+					.setFooter(`Aujourd'hui en Terre du Milieu • ${TDM.date}`);
+					message.channel.send(embed)
+					await delay(1800000);
+				}
+			}
+		message.reply("CHEH")    
+    },
+};
