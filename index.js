d4b46178068166be7282750f8d1c43b8295c716b
elrohir-bfme <elrohir.rassant@gmail.com>
2023-12-02 13:42:24 +0100

    up

--- index.js
+++ index.js
@@ -23,13 +23,13 @@ client.on("messageCreate", (message) => {
 	}
 
 	if(message.channelId == "646687718491029505") {
-		message.react('930543735580811307');
-		message.react('712627686698123265');
+		message.react('🇧🇪');
+		message.react('759764968043577354');
 	}
 
 	if(message.channelId == "1000403894083539055") {
-		message.react('712627686698123265');
-		message.react('930543735580811307');
+		message.react('🇧🇪');
+		message.react('759764968043577354');
 	}
 	
   });
